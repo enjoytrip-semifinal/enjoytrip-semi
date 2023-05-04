b4943b754634dbc57b5823e603c43363c5c25474
jungu12 <kjg070146@gmail.com>
2023-05-04 13:53:37 +0900

    0504 modify xml

--- src/main/java/com/enjoytrip/notice/model/NoticeDto.java
+++ src/main/java/com/enjoytrip/notice/model/NoticeDto.java
@@ -1,8 +1,12 @@
 package com.enjoytrip.notice.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+
 import lombok.Data;
 
 @Data
+@JsonAutoDetect(fieldVisibility = Visibility.ANY)
 public class NoticeDto {
 	private int noticeid;
 	private String registertime;
