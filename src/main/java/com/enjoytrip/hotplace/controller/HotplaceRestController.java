479f13be3ce570e355e2d8a06f369c6a72239e6f
sej <hara0822@naver.com>
2023-05-11 14:24:26 +0900

    editController

--- src/main/java/com/enjoytrip/hotplace/controller/HotplaceRestController.java
+++ src/main/java/com/enjoytrip/hotplace/controller/HotplaceRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -83,8 +84,9 @@ public class HotplaceRestController {
 		}
 	}
 	
-	
-	@PostMapping(value="list/{num}")
+	//
+	//@PostMapping(value="list/{num}")
+	@PutMapping(value = "list/{num}")
 	public ResponseEntity<?> listOneView(@PathVariable("num")Integer num, @RequestBody HotplaceDto hotplaceDto) throws Exception {
 		try {
 			HotplaceDto hotplace = service.getHotplace(num);
