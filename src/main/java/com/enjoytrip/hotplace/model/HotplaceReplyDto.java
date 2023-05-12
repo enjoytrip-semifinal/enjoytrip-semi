35afa4bbdf5615a0c628a565cdcf522bb84c6140
sej <hara0822@naver.com>
2023-05-12 15:24:57 +0900

    commit application.properties

--- src/main/java/com/enjoytrip/hotplace/model/HotplaceReplyDto.java
+++ src/main/java/com/enjoytrip/hotplace/model/HotplaceReplyDto.java
@@ -2,7 +2,8 @@ package com.enjoytrip.hotplace.model;
 
 public class HotplaceReplyDto {
 	private int replyId;
-	
+	private int userId;
+	private int hotplaceId;
 	private String content;
 	private String regtime;
 	
