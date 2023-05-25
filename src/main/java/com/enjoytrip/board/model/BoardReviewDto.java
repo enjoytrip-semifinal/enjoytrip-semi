417214aceaab504d70f2f80b98a18bce2003a21f
jjin <hara0822@naver.com>
2023-05-25 17:37:18 +0900

    add board reply nickname

--- src/main/java/com/enjoytrip/board/model/BoardReviewDto.java
+++ src/main/java/com/enjoytrip/board/model/BoardReviewDto.java
@@ -12,6 +12,7 @@ public class BoardReviewDto {
 	private int board_reply_id;
 	private String content;
 	private String register_date;
+	private String nickname;
 	private int board_id;
 	private int user_id;
 }
